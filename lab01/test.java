7afff79dfc8db652f2834e38db2cb2ebcdd0449a
Shayne Lewis <60145924+shayne-lewis@users.noreply.github.com>
2021-01-26 13:28:03 -0500

    Part 1 of lab02 with simple java program and gradle setup

--- lab01/test.java
+++ lab01/test.java
@@ -1,9 +1,6 @@
 //hi
 //changes to the file
 //hello again
-<<<<<<< HEAD
 //
 //changes for master branch
-=======
 //changed for beta branch
->>>>>>> beta
---
+++ lab02/main/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,9 @@
+package csci2020u.lab02;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
